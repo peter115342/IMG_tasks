67856d92af78a2993700a61bda405778b3e125f9
peter115342 <pmuzslay8@gmail.com>
2024-07-19 15:52:46 +0200

    More optimization

--- task2/task.php
+++ task2/task.php
@@ -1,32 +1,21 @@
 <?php
-public static function efficientMethod($user_id) {
-    $user = User::find($user_id);
-    if (!$user) {
-        return null;
-    }
+public static function efficientMethod($user_id)
+{
+    $user = User::findOrFail($user_id);
 
-    $courses = Course::where('active', 1)->get();
+    $courses = Course::where('active', 1)
+        ->with('certificate')
+        ->get();
     
-    $author_courses = [];
-    $all_certificates = [];
-    $phpCertificates = [];
-
-    foreach ($courses as $course) {
-        if ($course->author_id == $user->id) {
-            $author_courses[] = $course;
-        }
-
-        $certificate = $course->certificate;
-        $all_certificates[] = $certificate;
-
-        if (stripos($certificate->name, 'php') !== false) {
-            $phpCertificates[] = $certificate;
-        }
-    }
+    $author_courses = $courses->where('author_id', $user->id);
+    $all_certificates = $courses->pluck('certificate');
+    $phpCertificates = $all_certificates->filter(function ($certificate) {
+        return stripos($certificate->name, 'php') !== false;
+    });
 
     return [
-        'author_courses' => $author_courses,
-        'all_certificates' => $all_certificates,
-        'phpCertificates' => $phpCertificates
+        'author_courses' => $author_courses->values(),
+        'all_certificates' => $all_certificates->values(),
+        'phpCertificates' => $phpCertificates->values()
     ];
 }
